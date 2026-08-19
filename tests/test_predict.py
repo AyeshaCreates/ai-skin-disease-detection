1e5013331fb3e9024216547c3403556e4542e833
Ayesha <amairaangel33@gmail.com>
2026-08-19 19:47:31 +0530

    Update test_predict.py URL to new deployment

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -8,7 +8,7 @@ def main():
     img.save('test_skin.jpg')
     
     print("Testing /api/predict endpoint on the public URL...")
-    url = "https://4e7b136d242025d8-49-37-181-49.serveousercontent.com/api/predict"
+    url = "https://d6813907c08fe221-49-37-178-96.serveousercontent.com/api/predict"
     
     try:
         with open('test_skin.jpg', 'rb') as f:
